9b605fdae8c272553e18618e04fe3a20467e39fd
Tomas Votruba <tomas.vot@gmail.com>
2024-01-01 09:25:17 +0000

    init

---
+++ bin/rng.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Utils\ChangelogGenerator\Changelog\ChangelogContentsFactory;
+use Rector\Utils\ChangelogGenerator\Command\GenerateChangelogCommand;
+use Rector\Utils\ChangelogGenerator\GithubApiCaller;
+use Rector\Utils\ChangelogGenerator\GitResolver;
+use Symfony\Component\Console\Application;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$githubToken = getenv('GITHUB_TOKEN');
+$githubApiCaller = new GithubApiCaller($githubToken);
+
+// @todo
+
+$generateChangelogCommand = new GenerateChangelogCommand(
+    new GitResolver(),
+    $githubApiCaller,
+    new ChangelogContentsFactory()
+);
+
+$application = new Application();
+$application->add($generateChangelogCommand);
+$application->setDefaultCommand('generate-changelog', true);
+$application->run();
