bb33c7b988f82802a9c111939f8d627458e55a8d
Tomas Votruba <tomas.vot@gmail.com>
2025-10-27 13:01:51 +0100

    keep all lines, to avoid accidental removal

--- src/ChangelogContentsFactory.php
+++ src/ChangelogContentsFactory.php
@@ -16,23 +16,17 @@ final class ChangelogContentsFactory
      * @var array<ChangelogCategory::*, string[]>
      */
     private const FILTER_KEYWORDS_BY_CATEGORY = [
-        ChangelogCategory::SKIPPED => [
-            'fix wrong reference',
-            'enable phpstan',
-            'bump to phpstan',
-            'bump composer',
-            'cleanup phpstan',
-            'compatibility with betterreflection',
-            'update to',
-            '[automated]',
-            '[core]',
-            '[scope]',
-            '[scoper]',
-            '[scoped]',
-        ],
         ChangelogCategory::NEW_FEATURES => ['add', 'added', 'improve'],
         ChangelogCategory::BUGFIXES => ['fixed', 'fix'],
-        ChangelogCategory::REMOVED => ['removed', 'deleted', 'remove deprecated', 'remove', 'deprecated'],
+        ChangelogCategory::REMOVED => [
+            'removed',
+            'deleted',
+            'depre',
+            'deprecate',
+            'remove deprecated',
+            'remove',
+            'deprecated',
+        ],
     ];
 
     /**
@@ -48,25 +42,20 @@ final class ChangelogContentsFactory
             ChangelogCategory::NEW_FEATURES => [],
             ChangelogCategory::BUGFIXES => [],
             ChangelogCategory::REMOVED => [],
-            ChangelogCategory::SKIPPED => [],
         ];
 
         foreach ($changelogLines as $changelogLine) {
             foreach (self::FILTER_KEYWORDS_BY_CATEGORY as $category => $filterKeywords) {
-                if (! $this->isKeywordsMatch($filterKeywords, $changelogLine)) {
-                    continue;
-                }
+                if ($this->isKeywordsMatch($filterKeywords, $changelogLine)) {
+                    $linesByCategory[$category][] = $changelogLine;
 
-                $linesByCategory[$category][] = $changelogLine;
-                continue 2;
+                    continue 2;
+                }
             }
 
             // fallback to fixed
             $linesByCategory[ChangelogCategory::BUGFIXES][] = $changelogLine;
         }
-
-        // remove skipped lines
-        unset($linesByCategory[ChangelogCategory::SKIPPED]);
 
         // remove empty categories
         $linesByCategory = array_filter($linesByCategory);
